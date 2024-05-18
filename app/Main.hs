218b2e8ae60b0398c4f79cee85522c3a67327b4f
Aditya Ghidora <adityaghidora@gmail.com>
2024-05-18 19:55:24 +0530

    bind to 6379 port

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,19 @@
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module Main (main) where
 
-import Network.Simple.TCP (serve, HostPreference(HostAny), closeSock)
-
+import Network.Simple.TCP (HostPreference (HostAny), closeSock, serve)
 
 main :: IO ()
 main = do
-    -- You can use print statements as follows for debugging, they'll be visible when running tests.
-    putStrLn "Logs from your program will appear here"
+  -- You can use print statements as follows for debugging, they'll be visible when running tests.
+  putStrLn "Logs from your program will appear here"
 
-    -- Uncomment this block to pass stage 1
-    -- let port = "6379"
-    -- putStrLn $ "Redis server listening on port " ++ port
-    -- serve HostAny port $ \(socket, address) -> do
-    --     putStrLn $ "successfully connected client: " ++ show address
-    --     closeSock socket
+  -- Uncomment this block to pass stage 1
+  let port = "6379"
+  putStrLn $ "Redis server listening on port " ++ port
+  serve HostAny port $ \(socket, address) -> do
+    putStrLn $ "successfully connected client: " ++ show address
+    closeSock socket
